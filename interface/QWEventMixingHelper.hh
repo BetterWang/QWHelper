51020af4baa52573055ea0a7f015a4a541096453
Quan Wang <BetterWang@gmail.com>
2017-03-24 23:10:00 +0100

    [+] init

---
+++ interface/QWEventMixingHelper.hh
@@ -0,0 +1,31 @@
+
+// Mixing event helper class
+// events are sorted in vz and centrality ranges
+
+class QWEventMixingHelper {
+public:
+	explicit QWEventMixingHelper(std::vector<double> pz,
+			std::vector<double> pCent,
+			int	dept);
+	struct MEvent {
+		std::vector<double>	phi;
+		std::vector<double>	eta;
+		std::vector<double>	pT;
+		std::vector<double>	weight;
+	};
+	typedef	std::vector<MEvent>	vEvent;
+
+	int	push(double, double, MEvent);
+	vEvent*	get(double, double);
+
+private:
+	int	Nvz_;
+	int	Ncent_;
+	std::vector<double>	pz_;
+	std::vector<double>	pCent_;
+	int const		dept_;
+
+	// vEvent mix[Nvz][Ncent];
+	std::vector< std::vector< vEvent > >	mix;
+};
+
---
+++ src/QWEventMixingHelper.cc
@@ -0,0 +1,47 @@
+#include "QWAna/QWHelper/interface/QWEventMixingHelper.hh"
+
+QWEventMixingHelper::QWEventMixingHelper( std::vector<double> pz,
+			std::vector<double> pCent,
+			int	dept) :
+		pz_(pz),
+		pCent_(pCent),
+		dept_(dept)
+{
+	Nvz_ = pz_.size() -1 ;
+	Ncent_ = pCent_.size() -1;
+	mix.reserve(Nvz_);
+	for ( int i < 0; i < Nvz_; i++ ) {
+		mix[i].reserve(Ncent_);
+		for ( int j = 0; j < Ncent_; j++ ) {
+			mix[i][j].reserve(dept);
+		}
+	}
+}
+
+int QWEventMixingHelper::push(double vz, double cent, MEvent evt)
+{
+	int vzdis = std::distance(pz_.begin(), std::lower_bound(pz_.begin(), pz_.end(), vz)) - 1;
+	if ( vzdis < 0 or vzdis >= Nvz_ ) return -1;
+
+	int centdis = std::distance(pCent_.begin(), std::lower_bound(pCent_.begin(), pCent_.end(), cent)) - 1;
+	if ( centdis < 0 or centdis >= Ncent_ ) return -2;
+
+	if ( mix[vzdis][centdis].size() >= dept_ ) {
+		mix[vzdis][centdis].pop_front(evt);
+		mix[vzdis][centdis].push_back(evt);
+	} else {
+		mix[vzdis][centdis].push_back(evt);
+	}
+	return mix[vzdis][centdis].size();
+}
+
+vEvent* QWEventMixingHelper::get(double vz, double cent)
+{
+	int vzdis = std::distance(pz_.begin(), std::lower_bound(pz_.begin(), pz_.end(), vz)) - 1;
+	if ( vzdis < 0 or vzdis >= Nvz_ ) return nullptr;
+
+	int centdis = std::distance(pCent_.begin(), std::lower_bound(pCent_.begin(), pCent_.end(), cent)) - 1;
+	if ( centdis < 0 or centdis >= Ncent_ ) return nullptr;
+
+	return &mix[vzdis][centdis];
+}
